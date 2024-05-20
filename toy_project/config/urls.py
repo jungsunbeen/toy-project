382d47485a9f7fac195b3dd395cfe7fab9ad6dca
정선빈 <136215148+jungsunbeen@users.noreply.github.com>
2024-05-20 13:56:55 +0900

    Delete toy_project directory

--- toy_project/config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from guestbook.views import *
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('guestbook/', include('guestbook.urls')),
-]
